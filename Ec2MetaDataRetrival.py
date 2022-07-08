d44670b0901bfc96c3ca765abeb9e715f9dd75ea
bigbansal <bigbansal111@gmail.com>
2022-07-08 23:17:44 +0530

    Ec2 MetaData Retrival

---
+++ Ec2MetaDataRetrival.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import requests
+import json
+
+
+# Converts AWS EC2 instance metadata to a dictionary
+def load():
+    metaurl = 'http://169.254.169.254/latest'
+    # those 3 top subdirectories are not exposed with a final '/'
+    metadict = {'dynamic': {}, 'meta-data': {}, 'user-data': {}}
+
+    for subsect in metadict.keys():
+        crawl('{0}/{1}/'.format(metaurl, subsect), metadict[subsect])
+
+    return metadict
+
+
+def crawl(url, d):
+    r = requests.get(url)
+    if r.status_code == 404:
+        return
+
+    for l in r.text.split('\n'):
+        if not l: # "instance-identity/\n" case
+            continue
+        newurl = '{0}{1}'.format(url, l)
+        # a key is detected with a final '/'
+        if l.endswith('/'):
+            newkey = l.split('/')[-2]
+            d[newkey] = {}
+            crawl(newurl, d[newkey])
+
+        else:
+            r = requests.get(newurl)
+            if r.status_code != 404:
+                try:
+                    d[l] = json.loads(r.text)
+                except ValueError:
+                    d[l] = r.text
+            else:
+                d[l] = None
+
+if __name__ == '__main__':
+    val = raw_input("Enter your key with path (leave blank or type 'all' for all data): ")
+    try:
+        if val == 'all' or len(val) == 0:
+            print(json.dumps(load(), indent = 1))
+        else:
+            value = val.split('/')
+            allData = load()
+            keyLength = len(value)
+            valueToReturn = ''
+            try:
+                for i in range(keyLength):
+                    if keyLength == 1 or i == (keyLength-1):
+                        valueToReturn = allData.get(value[i])
+   
